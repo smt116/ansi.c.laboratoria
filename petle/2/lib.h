ab57e2767929377dfce526fb80dd9cc9357ca7fe
smt116 <smt116@gmail.com>
2011-03-23 14:03:25 +0100

    Zadanie 2 i 6 z petli (rozbudowane)

---
+++ petle/2/lib.h
@@ -0,0 +1,9 @@
+/*
+ * Maciej Małecki
+ * smt116@gmail.com
+ */
+#ifndef SMT__LOCAL_LIB
+#define SMT__LOCAL_LIB
+void wczytaj_dane(char *, int *, int *, char *);
+void drukuj_szachownice(int, int, char);
+#endif
