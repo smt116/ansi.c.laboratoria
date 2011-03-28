95d4da472707691e9d924a4f923f7ace111ddc32
smt116 <smt116@gmail.com>
2011-03-28 17:46:12 +0200

    Zadania 3, 4, 5, 6, 7, 8

--- petle/2/lib.c
+++ petle/2/lib.c
@@ -4,14 +4,12 @@
  */
 #include<stdio.h>
 
-void wczytaj_dane(char *prompt, int *x, int *y, char *znak)
-{
+void wczytaj_dane(char *prompt, int *x, int *y, char *znak) {
     printf(prompt);
     scanf("%i %i %c", x, y, znak);
 }
 
-void drukuj_szachownice(int x, int y, char znak)
-{
+void drukuj_szachownice(int x, int y, char znak) {
     unsigned i, k;
 
     for (i = 1; i <= y; i++) {
--- petle/2/main.c
+++ petle/2/main.c
@@ -4,15 +4,9 @@
  */
 #include "lib.h"
 
-int main()
-{
+int main() {
     int x, y;
     char znak;
-
-    /* dlaczego wymiary x y szachownicy?
-     * po co znak???....
-     * a no bo ja nie robie 
-     * destroy_bagdad - ja robie destroy_city! :D */
 
     wczytaj_dane("Podaj wymiar i znak, x y znak ", &x, &y, &znak);
     drukuj_szachownice(x, y, znak);
