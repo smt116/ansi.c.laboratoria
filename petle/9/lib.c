5bcaf8b6d0366c025ddd671873ace62d90bc123b
smt116 <smt116@gmail.com>
2011-04-02 22:30:57 +0200

    Zadanie 9 (petle): podzial lib.c na lib.c i file.c

--- petle/9/lib.c
+++ petle/9/lib.c
@@ -4,47 +4,16 @@
  */
 #include "lib.h"
 
-int wywolanie(int kod_bledu) {
-    switch(kod_bledu) {
-	case 2: printf("Za duzo argumentow\nPoprawne wywolanie: ./main plik\n",
-				    strerror(errno)); return 0; break;
-	case 1: printf("Plik nieistnieje\n",
-				strerror(errno)); return 0; break;
-	default: return 0;
-    }
-}
-
-int otworz_plik(FILE ** in_handle, int *argc, char *argv[1]) {
-    *in_handle = fopen(argv[0], "r");
-    if (*argc != 2) {
-	wywolanie(2);
-	return 0;
-    } else {
-	if(*in_handle == NULL) {
-	    wywolanie(1);
-	    return 0;
-	} else {
-	    return 1;
-	}
-    }
-}
-
-void zamknij_plik(FILE ** in_handle) {
-    /* osobna funkcja jako furtka
-     * do pozniejszej rozbudowy */
-    fclose(*in_handle);printf("ok");
-}
-
 void wczytaj_dane(FILE ** in_handle, unsigned *kilometry, double *litry,
-		  double *cena) {
+				  double *cena) {
     char bufor[BUFSIZ];
     short komentarz;
 
     while (fgets(bufor, BUFSIZ, *in_handle)) {
-	komentarz = sscanf(bufor, "%u %lf %lf", kilometry, litry, cena);
-	if(komentarz==3) {
-	    oblicz(*kilometry, *litry, *cena);
-	}
+		komentarz = sscanf(bufor, "%u %lf %lf", kilometry, litry, cena);
+		if(komentarz==3) {
+	    	oblicz(*kilometry, *litry, *cena);
+		}
     }    
 }
 
--- petle/9/lib.h
+++ petle/9/lib.h
@@ -7,8 +7,12 @@
 
 #include<stdio.h>
 #include<errno.h>
+
+/* lib.c: */
 int wywolanie(int);
 int otworz_plik(FILE ** in_handle, int *, char *argv[1]);
+
+/* file.c: */
 void zamknij_plik(FILE ** in_handle);
 void wczytaj_dane(FILE ** in_handle, unsigned *, double *, double *);
 void oblicz(unsigned, double, double);
