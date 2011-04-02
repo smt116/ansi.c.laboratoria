7b3a70da257ddc2afc95872f848bcbfa7a28b67e
smt116 <smt116@gmail.com>
2011-04-02 16:48:57 +0200

    Zadanie 3 (operatory): kompletny, dzialajacy program

---
+++ operatory/3/lib.c
@@ -0,0 +1,20 @@
+/*
+ * Maciej Małecki
+ * smt116@gmail.com
+ */
+#include <stdio.h>
+
+void wczytaj_liczby(int *a, int *b, char *prompt) {
+	printf(prompt);
+	scanf("%i %i", a, b);
+}
+
+void suma(int a, int b, char *prompt) {
+	int suma = 0, tmp = a;
+	do {
+		suma += tmp;
+		tmp++;
+	} while(tmp<=b);
+
+	printf(prompt, a, b, suma);
+}
---
+++ operatory/3/main.c
@@ -0,0 +1,14 @@
+/*
+ * Maciej Małecki
+ * smt116@gmail.com
+ */
+#include "lib.h"
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	
+	wczytaj_liczby(&a, &b, "Podaj dwie liczby calkowite. a b = ");
+	suma(a, b, "Suma liczb w przedziale ( %i,%i ) wynosi %i\n");
+    return 0;
+}
