965db24902f55100ba95d0ffb29ed926f4efc9d5
smt116 <smt116@gmail.com>
2011-04-03 10:37:54 +0200

    Zadanie 1 (operatory): kompletny, dzialajacy program

--- operatory/1/lib.c
+++ operatory/1/lib.c
@@ -2,3 +2,28 @@
  * Maciej Małecki
  * smt116@gmail.com
  */
+#include <stdio.h>
+#define GODZINA 60
+
+void pobierz_ilosc(int *ilosc, char *prompt) {
+    printf(prompt);
+    scanf("%i", ilosc);
+}
+
+void ile_jednostki(int ilosc, char znak, char *jednostka) {
+    int ile;
+
+    switch (znak) {
+    case '/':
+	ile = ilosc / GODZINA;
+	break;
+    case '%':
+	ile = ilosc % GODZINA;
+	break;
+    default:
+	ilosc = 0;
+	break;
+    }
+
+    printf("%i%s ", ile, jednostka);
+}
--- operatory/1/main.c
+++ operatory/1/main.c
@@ -4,8 +4,18 @@
  */
 #include "lib.h"
 
-int main()
-{
+int main() {
+    int ilosc;
+
+    do {
+	pobierz_ilosc(&ilosc, "Podaj ilosc minut: ");
+	if (!(ilosc > 0))
+	    break;
+
+	ile_jednostki(ilosc, '/', "h");
+	ile_jednostki(ilosc, '%', "m");
+	putchar('\n');
+    } while (ilosc > 0);
 
     return 0;
 }
