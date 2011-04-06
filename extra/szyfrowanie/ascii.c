8d45d66913932191c7b3699d2044a6586ca5729d
smt116 <smt116@gmail.com>
2011-04-06 21:50:43 +0200

    Zadanie szyfrowanie (extra): dodanie funkcji zliczajacej ilosc liter a-z A-Z w pliku ascii.c i usuniecie funkcji zamieniajacej ascii na tekst

--- extra/szyfrowanie/ascii.c
+++ extra/szyfrowanie/ascii.c
@@ -14,8 +14,19 @@ void tekst_na_ascii(short *tekst) {
 }
 
 void zlicz_litery(short *tekst) {
-    unsigned i = 0, ilosc[26] = {};
+    /* poki co zliczam kazdy znak z ascii 
+       potem poprawie to ;) */
+    unsigned i = 0, j, ilosc[127] = { };
+
     do {
+	ilosc[tekst[i]]++;
 	i++;
-    } while(tekst[i + 1] != EOF);
+    } while (tekst[i + 1] != EOF);
+
+    for (i = 65, j = 0; i <= 122; i++, j++) {
+	printf("%c %i\t", (char) i, ilosc[i]);
+	if (i == 90) { i = 96; printf("\n"); }
+	if (j%5==0) printf("\n");
+    }
+    printf("\n");
 }
--- extra/szyfrowanie/main.c
+++ extra/szyfrowanie/main.c
@@ -11,12 +11,12 @@ int main() {
 
     wczytaj_tekst(tekst, "Podaj tekst wejsciowy (EOF konczy procedure):\n");
     wyswietl_tekst(tekst, "\nPodany tekst: \n");
-    
+
     opcja = wybor_opcji("\nCo chcesz zrobic?:\n\t1. Zamien tekst na ASCII\n\t2. Zlicz ilosc poszczegolonych liter\nWybor: ");
 
     switch (opcja) {
         case 1: tekst_na_ascii(tekst); break;
-	case 2: zlicz_litery(tekst); break;
+        case 2: zlicz_litery(tekst); break;
         default: printf("blad");
     }
     return 0;
