094a4f680499864a077219292065161dd0d3eab7
smt116 <smt116@gmail.com>
2011-04-06 14:39:57 +0200

    Zadanie szyfrowanie (extra): skopiowanie wszystkich plikow do folderu tmp, z tego wzgledu, ze chce inaczej zrealizowac zadanie

---
+++ extra/szyfrowanie/tmp/lib.c
@@ -0,0 +1,38 @@
+/*
+ * Maciej Małecki
+ * smt116@gmail.com
+ */
+#include "lib.h"
+
+void wczytaj_tekst(short *tekst, char *prompt) {
+    int i = 0;
+    char znak;
+    printf(prompt);
+
+    do {
+	znak = getchar();
+	tekst[i] = (int) znak;
+	i++;
+    } while (znak != EOF);
+}
+
+void wyswietl_tekst(short *tekst, char *prompt) {
+    int i = 0;
+    printf(prompt);
+
+    do {
+	printf("%c", (char) tekst[i]);
+	i++;
+    } while (tekst[i] != EOF);
+}
+
+short menu(char *prompt) {
+    short opcja;
+    printf(prompt);
+
+    do {
+	scanf("%hi", &opcja);
+    } while (opcja <= 0 || opcja >= 3);
+
+    return opcja;
+}
---
+++ extra/szyfrowanie/tmp/lib.h
@@ -0,0 +1,25 @@
+/*
+ * Maciej Małecki
+ * smt116@gmail.com
+ */
+#ifndef SMT__LOCAL_LIB
+#define SMT__LOCAL_LIB
+
+#include <stdio.h>
+
+/* lib.c: */
+void wczytaj_tekst(short *, char *);
+void wyswietl_tekst(short *, char *);
+short menu(char *);
+
+/* ascii.c: */
+void ascii_na_tekst(short *);
+void tekst_na_ascii(short *);
+
+/* czestosc.c:*/
+
+/* szyfr_cezara.c: */
+
+/* szyfr_lioniowy.c: */
+
+#endif
